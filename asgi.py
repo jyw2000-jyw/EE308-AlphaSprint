397f5a1f75142e38a8014104653aa21283c5df7c
OLEGShen <76512168+OLEGShen@users.noreply.github.com>
2021-11-13 22:17:13 +0800

    Update asgi.py

--- asgi.py
+++ asgi.py
@@ -4,7 +4,13 @@ ASGI config for test_one project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
+
+
+
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+
+
+
 """
 
 import os
