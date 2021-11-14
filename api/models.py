d6edd90e7d65fece8b69798a84024c1db3d2b350
OLEGShen <76512168+OLEGShen@users.noreply.github.com>
2021-11-14 15:56:39 +0800

    Add files via upload

---
+++ api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from datetime import datetime
+
+class api(models.Model):
+    #id = models.AutoField(primary_key=True) #主键可省略不写
+    name = models.CharField(max_length=32)
+    age = models.IntegerField(default=20)
+    phone = models.CharField(max_length=16)
+    addtime=models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.name+":"+self.phone
+
+    class Meta:
+       db_table="myapp_users"
